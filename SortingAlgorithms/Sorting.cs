5af4e6efde19202fff08c40aeb91438d749edb9c
Yahmenev Roman <jvai@yandex.ru>
2015-06-24 14:29:45 +0300

    Selection sort is done

--- SortingAlgorithms/Sorting.cs
+++ SortingAlgorithms/Sorting.cs
@@ -8,6 +8,31 @@ namespace SortingAlgorithms {
 
     class Sorting {
 
+        public static int[] SelectionSort(int[] array)
+        {
+            int arrayLength = array.Length;
+            int min = 0;
+
+            for (int i = 0; i <= arrayLength - 1; i++) {
+
+                min = array[i];
+
+                for (int j = i; j <= arrayLength - 1; j++) 
+                {
+                    if ()
+
+                    if (array[j] < min) 
+                    { 
+                        min = array[j];
+                        array[j] = array[i];
+                        array[i] = min;
+                    }
+                }
+            }
+
+            return array;
+        }
+
         public static int[] GenerateArrayRandomNumbers(int numbersCount = 30)
         {
             int[] array = new int[numbersCount];
